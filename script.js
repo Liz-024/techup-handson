c77e45bd84d30fd7ba67665b2765bf38cf750b17
Liz-024 <helizabeth.yf06@gmail.com>
2024-11-04 08:18:54 +0000

    JS Tryout

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
-/* Variable: is a holder for data 
-Common data types: 
-    String [stores text e.g.,"xx"], 
-    Number [1,2,3...],
-        1.5 = float 
-    Boolean [true/false], 
-    Array [Collection of data - ordered. Order starts from 0]
-    In JS: end all lines with ;
-    */
+var age = 20;
+age = 65;
+console.log(age);
 
-    /* Conditionals: if, if-else
-    If (condition == x) {Action/result}
-    Use == in conditional, not =
-    */
+var username = "bob";
+var password = "techup";
+var isMale = true;
+var isLoggedin = false;
+var blog_posts = ["hello, how are you?","i am well", "thank you!","the end"];
 
+console.log(username)
+console.log(password)
+console.log(isMale)
+console.log(isLoggedin)
+console.log(blog_posts)
+
+if (isMale == true){
+    console.log("You are male")
+} else {
+    console.log("You are not male");
+}
+
+while (age <70){
+    console.log("your age is " + age);
+    age = age + 1;
+}
+
+function sayHello() {
+        var x = 1;
+        while (x <5) {alert ("Hello everyone!");
+        x=x+1;
+        }  
+}
+
+function showAge() {
+    var user_input = document.getElementById("user-input").value
+    document.getElementById("header-age").textContent = user_input
+}
